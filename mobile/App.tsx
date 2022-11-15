cd72714c9ff07f151f777953376ec060790b44e3
Guilherme Takahashi <gui.yukio175@gmail.com>
2022-11-14 23:46:50 -0300

    feat: context of authentication

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,8 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { AuthContextProvider } from './src/contexts/AuthContext';
+
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
 
@@ -11,16 +13,18 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {
-        fontsLoaded 
-          ? <SignIn />
-          : <Loading />
-      }
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {
+          fontsLoaded 
+            ? <SignIn />
+            : <Loading />
+        }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,34 @@
-import { Center, Text } from 'native-base';
+import { Center, Icon, Text } from 'native-base';
+import { Fontisto } from '@expo/vector-icons';
+
+import { useAuth } from '../hooks/useAuth';
+
+import { Button } from '../components/Button';
+import Logo from '../assets/logo.svg';
 
 export function SignIn() {
+  const { signIn, user } = useAuth();
+
+  console.log('Dados do usuário => ', user)
+
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24}>
-        SignIn
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      
+      <Button 
+        title='Entrar com google' 
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        type='SECONDARY'
+        mt={12}
+        onPress={signIn}
+      />
+
+      <Text
+        color="white"
+        textAlign="center"
+        mt={4}
+      >
+        Não utilizamos nenhuma informação além do seu e-mail para criação de sua conta
       </Text>
     </Center>
   )
